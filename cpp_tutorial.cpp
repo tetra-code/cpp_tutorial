e286a36505e007668b5464ff7c0d3d64aec92019
Pil Kyu Cho <comsci.p.cho@gmail.com>
2022-09-09 21:22:01 +0200

    added cpp files

---
+++ cpp_tutorial.cpp
@@ -0,0 +1,14 @@
+// cpp_tutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+// In the future, to open this project again, go to File > Open > Project and select the .sln file
